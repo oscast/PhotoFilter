d03a8096ff640ba62f6959e8afcb53e394a98fa2
Oscar Castillo <goldoac@live.com>
2024-10-02 21:53:41 -0600

    Initial Commit

---
+++ PhotoFilter/PhotoFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFilterApp.swift
+//  PhotoFilter
+//
+//  Created by Oscar Castillo on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
